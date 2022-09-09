1c297d3d44e0513ee50e8c0525942ecfb7726885
notreallyme2 <lindsay.m.edwards@gmail.com>
2022-09-09 11:40:17 +0100

    Karate chop tests working

---
+++ karate-chop/karate_chop.py
@@ -0,0 +1,5 @@
+import typing
+from typing import Any
+
+def chop(search_value: Any, search_list: list) -> int:
+    pass
---
+++ karate-chop/test_karate_chop.py
@@ -0,0 +1,26 @@
+# tests from CodeKata website
+
+from karate_chop import chop
+
+def test_chop():
+  assert -1 == chop(3, [])
+  assert -1 == chop(3, [1])
+  assert 0 == chop(1, [1])
+  #
+  assert 0 == chop(1, [1, 3, 5])
+  assert 1 == chop(3, [1, 3, 5])
+  assert 2 == chop(5, [1, 3, 5])
+  assert -1 == chop(0, [1, 3, 5])
+  assert -1 == chop(2, [1, 3, 5])
+  assert -1 == chop(4, [1, 3, 5])
+  assert -1 == chop(6, [1, 3, 5])
+  #
+  assert 0 == chop(1, [1, 3, 5, 7])
+  assert 1 == chop(3, [1, 3, 5, 7])
+  assert 2 == chop(5, [1, 3, 5, 7])
+  assert 3 == chop(7, [1, 3, 5, 7])
+  assert -1 == chop(0, [1, 3, 5, 7])
+  assert -1 == chop(2, [1, 3, 5, 7])
+  assert -1 == chop(4, [1, 3, 5, 7])
+  assert -1 == chop(6, [1, 3, 5, 7])
+  assert -1 == chop(8, [1, 3, 5, 7])
