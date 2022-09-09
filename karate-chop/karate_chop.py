ab90c28f73a8f72f488759eb9169fe84457b499a
notreallyme2 <lindsay.m.edwards@gmail.com>
2022-09-09 14:47:58 +0100

    Used the solution from Wengrow

--- karate-chop/karate_chop.py
+++ karate-chop/karate_chop.py
@@ -8,19 +8,17 @@ def chop(search_value: Any, search_list: list) -> int:
     if len(search_list) == 0:
         return -1
 
-    lower_bound = 0; upper_bound = len(search_list)
-    not_found = True
+    lower_bound = 0; upper_bound = len(search_list) - 1
 
-    while not_found == True:
-        search_index = lower_bound + (upper_bound - lower_bound) // 2
-        print (lower_bound, upper_bound, search_index)
-        if search_list[search_index] == search_value:
-            not_found = False
-            return search_index
-        if search_list[search_index] > search_value:
-            upper_bound = search_index - 1
-        if search_list[search_index] < search_value:
-            lower_bound = search_index + 1
-        if lower_bound == upper_bound: #or lower_bound > upper_bound:
-            not_found = False
-            return -1
+    while lower_bound <= upper_bound:
+        midpoint = (lower_bound + upper_bound) // 2
+        value_at_midpoint = search_list[midpoint]
+        # print (lower_bound, upper_bound, search_index)
+        if value_at_midpoint == search_value:
+            return midpoint
+        elif value_at_midpoint > search_value:
+            upper_bound = midpoint - 1
+        elif value_at_midpoint < search_value:
+            lower_bound = midpoint + 1
+
+    return -1
